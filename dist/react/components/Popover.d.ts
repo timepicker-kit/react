c051cbe124fd940875e683f523314729e24c12fb
David Jaja <84209542+Daiveedjay@users.noreply.github.com>
2024-12-20 16:47:41 +0100

    Enhance Popover component with additional styling and functionality; update package.json for build scripts and add global CSS variables.

--- dist/react/components/Popover.d.ts
+++ dist/react/components/Popover.d.ts
@@ -2,6 +2,7 @@ import { PopoverContentProps, SharedPopoverTypes, TimeInput } from '../types/typ
 export declare function Popover({ children, className }: SharedPopoverTypes): import("react/jsx-runtime").JSX.Element;
 export declare const PopoverTrigger: ({ children, className }: SharedPopoverTypes) => import("react/jsx-runtime").JSX.Element;
 export declare const PopoverContent: ({ children, className, isAnimate, variants, }: PopoverContentProps) => import("react/jsx-runtime").JSX.Element | null;
-export declare const PopoverButton: ({ time }: {
+export declare const PopoverButton: ({ time, className, }: {
     time: TimeInput;
+    className?: string;
 }) => import("react/jsx-runtime").JSX.Element;
